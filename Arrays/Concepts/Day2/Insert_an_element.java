4e5176824d0d2f8cc5775dfc9daa6e373788bd82
Archana Nair <nairarchana2004@gmail.com>
2026-09-17 14:34:00 +0530

    Insert an element

--- Arrays/Concepts/Day2/Insert_an_element.java
+++ Arrays/Concepts/Day2/Insert_an_element.java
@@ -1,3 +1,42 @@
+// INPUT ARRAY IN THE CODE 
+public class Main{
+    public static void main(String[] args){
+        // Input array
+        int[] arr = {10,20,30,40};
+        // n holds the length of the array
+        int n = arr.length;
+// Decide what to insert and where to insert it
+// position holds the value of 2
+        int position = 2;
+        // value holds the value of 25
+        int value = 25;
+
+// Creating a bigger array to hold the new element
+        int[] newArr = new int[ n + 1];
+        // n+1 because we are inserting only one element into the array
+
+
+        for (int i=0;i< position;i++){
+            // Copy everything that comes before position 2
+            newArr[i] = arr[i];
+        }
+        // Insert the new value at the specified position
+        newArr[position] = value;
+        
+        // Shifting the remaining elements
+        for(int i = position; i < n; i++){
+            newArr[i+1] = arr[i];
+            // i+1 because we need to move each remaining element one step forward
+        }
+        // Print the new array after insertion
+        for(int x: newArr){
+            System.out.print(x + " ");
+        }
+    }
+}
+
+
+// USER INPUT ARRAY AND POSITION 
 import java.util.Scanner;
 
 public class Main {
